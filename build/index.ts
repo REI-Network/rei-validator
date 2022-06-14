96e116b3009cd089c74a6dde72daa2ed3498c520
haifun <haifuncn@gmail.com>
2022-06-14 15:46:43 +0800

    first commit

---
+++ build/index.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs'
+const path = require('path');
+
+const validator = path.resolve(__dirname, '../src/validator');
+
+const jsonFileNames = fs.readdirSync(validator);
+
+const jsonFileNameFilter = jsonFileName => {
+  return jsonFileName !== '$template.json' && jsonFileName.endsWith('.json')
+}
+const arr = []
+
+jsonFileNames
+  .filter(jsonFileNameFilter)
+  .forEach(jsonFileName => {
+    // @ts-ignore
+   const content = fs.readFileSync(`${validator}/${jsonFileName}`,'utf-8').toString()
+    arr.push(JSON.parse(content))
+  })
+
+const obj = {
+  data: arr
+}
+
+const data = new Uint8Array(Buffer.from(JSON.stringify(obj)));
+
+const dir = './dist';
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+const dist = path.resolve(__dirname, '../dist')
+
+fs.writeFile(dist+'/validator-list.json', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
+
